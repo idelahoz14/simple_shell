9e0f78bfa9f240814c0e221d433215797910e7fc
ClaraCastaD <1980@holbertonschool.com>
2020-08-12 23:24:30 +0000

    Adding the readline and split files

---
+++ readline.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *read_line -  functions that reads the line
+  *Return: line
+  */
+char *read_line()
+{
+	char *line = NULL;
+	size_t size = 0;
+	int i;
+
+	i = getline(&line, &size, stdin);
+
+	if (i == EOF)
+	{
+		free(line)
+		return (NULL);
+	}
+	return (line);
+}
---
+++ split.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+/**
+  *split - function that splits a string
+  *@line: pointer
+  *Return: array
+  */
+char **split(char *line)
+{
+	char *token = strtok(line, " ");
+	char **nest = malloc((sizeof(char *)) * (1024));
+	int i = 0;
+
+	nest[i] = token;
+	i++;
+
+	while (token != NULL)
+	{
+		nest[i] = token;
+		i++;
+		token = strtok(NULL, " ");
+	}
+	nest[i] = NULL;
+	return (nest);
+}
