afb97a6a541c58e28e80874cf7eaf24010d6e595
idelahoz14 <idelahzo14@cuc.edu.co>
2020-08-14 01:51:31 +0000

    Function main

---
+++ main_prueba.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * main - shell project
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *line;
+	char **array;
+
+	do{
+		printf("$ ");
+		line = read_line();
+		if (!_strcmp(line, "exit"))
+		{
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+		array = split(line);
+		process(array);
+	}while(1);
+	return (0);
+}
